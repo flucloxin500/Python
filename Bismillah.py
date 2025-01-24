c193ab64f824c9edb6e7c2cb7f0141c58abc83e6
flucloxin500 <mdwalid826@gmail.com>
2025-01-24 19:36:36 +0600

    first commit

---
+++ Bismillah.py
@@ -0,0 +1,23 @@
+#Byte/ByteArray
+# list = [1,2,6,5,4,5,8,41,2,6,85,4,52,68,4,60]
+# b = bytearray(list)
+# b[0] = 10
+# print(b)
+
+# x = None
+# print(type(x))
+
+# li = ["Walid" , 22 , 25500.25]
+# li[2]="Lixam"
+# print(li)
+
+# tup = (5,10,15,20,25)
+
+# print(type(tup))
+
+ran = range(50)
+
+print(ran)
+
+for k in ran:
+    print(k)
