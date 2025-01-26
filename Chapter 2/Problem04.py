52bd2ef3be658b0b5070cd75e9ae9e06d6ac4db5
flucloxin500 <mdwalid826@gmail.com>
2025-01-26 15:42:19 +0600

    Chapter 2

--- Chapter 2/Problem04.py
+++ Chapter 2/Problem04.py
@@ -1,9 +1,9 @@
 #Use comparison operator to find out whether ‘a’ given variable a is greater than ‘b’ or not. Take a = 34 and b = 80 
 
 a = 34 
-b = 80
+b = 80 
 
 c = a < b 
 #c = a < b 
 
-print(c)
+print(c) 
