56014e2e913e6d3f0b795c410d69fb2e76f9e3c5
flucloxin500 <mdwalid826@gmail.com>
2025-02-01 12:47:58 +0600

    Chapter 9

---
+++ Chapter 9/01_file_read.py
@@ -0,0 +1,7 @@
+f = open(r"E:\Python\All Projects\Chapter 9\file.txt", "r")
+
+data = f.read()
+
+print(data)
+
+f.close()
---
+++ Chapter 9/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+st = "Hey walid, You are amazing!"
+
+f = open(w"E:\Python\All Projects\Chapter 9\file.txt", "w")
+
+f.write(st)
+
+f.close()
