29b28f82f3f1786e006d19afbf9328930fc821c9
flucloxin500 <mdwalid826@gmail.com>
2025-02-03 15:55:54 +0600

    Chapter 9

--- Chapter 9/Problem05.py
+++ Chapter 9/Problem05.py
@@ -1,1 +1,14 @@
 '''Repeat program 4 for a list of such words to be censored.'''
+
+words = ["Doneky","bad","crucial","rude"]
+
+with open(r"E:\Python\All Projects\Chapter 9\file.txt", "r") as f:
+    content = f.read()
+
+for word in words :
+    content = content.replace(word, "#" * len(word))
+
+
+
+with open(r"E:\Python\All Projects\Chapter 9\file.txt", "w") as f:
+    f.write(content)
