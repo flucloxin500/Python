3b7749d39944294dd2cd94936d33915009f5b87f
flucloxin500 <mdwalid826@gmail.com>
2025-02-07 15:15:09 +0600

    Chapter 9

--- Chapter 9/Problem11.py
+++ Chapter 9/Problem11.py
@@ -1,1 +1,7 @@
 '''Write a python program to rename a file to “renamed_by_ python.txt. '''
+
+with open (r"E:\Python\All Projects\CHapter 9\old.txt","r") as f:
+    content = f.read()
+    
+with open (r"E:\Python\All Projects\CHapter 9\renamed_by_python.txt","w") as f:
+    f.write(content)
