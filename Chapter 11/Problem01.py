8604132e12cad557592bfe4edf9c6b309c8d51eb
flucloxin500 <mdwalid826@gmail.com>
2025-02-12 23:20:37 +0600

    Chapter 11

--- Chapter 11/Problem01.py
+++ Chapter 11/Problem01.py
@@ -1,6 +1,26 @@
 ''' Create a class (2-D vector) and use it to create another class representing a 3-D 
 vector. '''
 
+class TwoDVector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def show(self):
+        print(f"The Vector is {self.x}i + {self.y}j")
+        
+class ThreeDVector(TwoDVector):
+    def __init__(self, x, y, z):
+        #super().__init__(x,y)
+        self.x = x
+        self.y = y
+        self.z = z
+        
+    def show(self):
+        print(f"The Vector is {self.x}i + {self.y}j + {self.z}k")
+        
 
-
-
+a = TwoDVector(1,2)
+a.show()
+b=ThreeDVector(1,2,3)
+b.show()
--- Chapter 11/Problem06.py
+++ Chapter 11/Problem06.py
@@ -2,3 +2,16 @@
 7i + 8j +10k  
 Assume vector of dimension 3 for this problem. '''
 
+class Vector:
+    def __init__(self,l):
+        self.l = l
+        
+    
+    def __len__(self):
+        return len(self.l)
+    
+
+v1 = Vector([1,2,3])
+
+print(len(v1))
+
